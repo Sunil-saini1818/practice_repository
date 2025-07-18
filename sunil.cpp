b61ac0b77fb0c9a9170d5e24150d9848cbc12dd0
Sunil-saini1818 <sainisunil1818@gmail.com>
2025-07-18 16:28:16 -0700

    sunil's c++ program

---
+++ sunil.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout<<"Enter value of a and b";
+    cin>>a;
+    cin>>b;
+    if(a>b){
+        cout<<"a is greater";
+
+    }
+    else{
+        cout<<"b is greater";
+    }
+    return 0;
+
+}
